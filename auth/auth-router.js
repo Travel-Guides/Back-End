6c0aa47ae74c3a398bf3c15f8d493e8d2baacf12
Darren J Carrillo <darrenjcarrillo@gmail.com>
2019-12-02 21:16:37 -0800

    Guides & Tourists Router Created

---
+++ auth/auth-router.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const { validateUser } = require("./validate-user.js");
+
+router.post("/guides/register", (req, res) => {
+  let user = req.body;
+  const validateResult = validateUser(user);
+  console.log(validateResult);
+
+  if (validateResult.isSuccessful === true) {
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash;
+    console.log(user);
+  }
+});
--- auth/validate-user.js
+++ auth/validate-user.js
@@ -1,10 +1,20 @@
 module.exports = {
-  
-}
-
+  validateUser
+};
 
 function validateUser(user) {
   let errors = [];
 
-  if(!user.username || user.username.length)
-} 
+  if (!user.username || user.username.length) {
+    errors.push("Please include a username with at least 4 characters");
+  }
+
+  if (!user.password || user.password.length) {
+    errors.push("Please include a password with at least 6 characters");
+  }
+
+  return {
+    isSuccessful: errors.length > 0 ? false : true,
+    errors
+  };
+}
