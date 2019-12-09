c8efe2bb4ec0e6e2a421cc883ae16ef38fa9899c
Darren J Carrillo <darrenjcarrillo@gmail.com>
2019-12-08 17:40:25 -0800

    Routers created for trips/guides/tourists

--- routers/guides/guides-router.js
+++ routers/guides/guides-router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const Guides = require("./guides-model.js");
+const Trips = require("../trips/trips-model.js");
 const authenticate = require("../../auth/restricted-middleware.js");
 
 const router = express.Router();
@@ -23,3 +24,31 @@ router.get("/guides/:id", (req, res) => {
       res.status(500).json({ error: "Failed to get guides" });
     });
 });
+
+router.get("/guides/:id/trips", authenticate, (req, res) => {
+  const { id } = req.params;
+
+  Trips.getTripsByGuideId(id)
+    .then(list => {
+      if (list.length) {
+        res.status(200).json(list);
+      } else {
+        res
+          .status(404)
+          .json({ message: "Could not find list for given scheme " });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: err.message });
+    });
+});
+
+router.post("/guides/:id/trips", authenticate, (req, res) => {
+  const { id } = req.params;
+
+  Trips.addTripsByGuideId(req.body, id)
+    .then(trips => res.status(200).json(trips))
+    .catch(err => res.status(500).json({ message: err.message }));
+});
+
+module.exports = router;
---
+++ routers/trips/trips-router.js
@@ -0,0 +1,53 @@
+const express = require("express");
+
+const Trips = require("./trips-model");
+
+const authenticate = require("../../auth/restricted-middleware.js");
+
+const router = express.Router();
+
+router.get("/", authenticate, (req, res) => {
+  Trips.find()
+    .then(trips => res.status(200).json(trips))
+    .catch(err => res.status(500).json({ error: err }));
+});
+
+router.get("/:id", authenticate, (req, res) => {
+  Trips.findById(req.params.id)
+    .then(trip => {
+      if (trip) {
+        res.status(200).json(trip);
+      } else {
+        res.status(400).json({ message: "Trip doesn't exist" });
+      }
+    })
+    .catch(err => res.status(500).json({ error: err }));
+});
+
+router.post("/", (req, res) => {
+  Trips.add(req.body)
+    .then(trips => res.status(200).json(trips))
+    .catch(err => res.status(500).json({ error: err }));
+});
+
+router.put("/:id", authenticate, (req, res) => {
+  Trips.findById(req.params.id)
+    .then(trip => {
+      if (trip) {
+        Trips.update(req.body, req.params.id).then(updated =>
+          res.json(updated)
+        );
+      } else {
+        res.status(404).json({ message: "Trip doesnt exist" });
+      }
+    })
+    .catch(err => res.status(500).json({ error: err }));
+});
+
+router.delete("/:id", authenticate, (req, res) => {
+  Trips.remove(req.params.id)
+    .then(trip => res.json({ message: `${trip} has been deleted` }))
+    .catch(err => res.status(500).json({ error: err }));
+});
+
+module.exports = router;
